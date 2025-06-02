c32d8d5905da3fd2783e6c8b8299a78bc9ba1010
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-06-02 16:03:56 -0300

    atualizar

--- aula-2/index.js
+++ aula-2/index.js
@@ -1,1 +1,17 @@
-console.log('Oi');
+let nome = 'João'; //String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome ,'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+
+let sobrenome; // Declarou a variável 
+sobrenome = 'Lima' // Inicializando a variável 
+
+console.log(sobrenome)
+
+sobrenome = 'oliveira'
+
+console.log(sobrenome);
