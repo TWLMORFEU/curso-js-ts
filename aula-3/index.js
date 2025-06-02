cbcc4a6d32891ebf1308e885f746e85769b49a79
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-06-02 18:23:20 -0300

    Update index.js

--- aula-3/index.js
+++ aula-3/index.js
@@ -13,10 +13,14 @@ const primeiroNumero = 5;
 
 const segundoNumero = 10; 
 
-const resultado = primeiroNumero * segundoNumero;
+const resultado = primeiroNumero * segundoNumero; 
 
-console.log(resultado);
+console.log (resultado)
 
 const resultadoDuplicado = resultado / 2; 
 
 console.log (resultadoDuplicado)
+
+const resultadoTriplicado = resultadoDuplicado + resultado 
+
+console.log (resultadoTriplicado)
