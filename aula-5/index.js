a6722a714530c75a025c5c8bc9fbbbc2117ea6fb
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-06-03 16:58:59 -0300

    atualizar fim de aula

--- aula-5/index.js
+++ aula-5/index.js
@@ -1,3 +1,4 @@
+// String, Number, underfined, null, boolean, symbol
 const nome = 'Luiz'; //string
 const nome1 = 'Luiz'; //string
 const nome2 = 'Luiz'; //string
@@ -5,3 +6,11 @@ const number1 = 10; //Number
 const number2 = 10.40; //Number
 let nomeAluno; //undefined -> não aponta para lugar nenhum na memória
 let sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum na memória 
+
+let a = 2;
+const b = a;
+
+console.log(a, b); // 2,2
+
+a = 3;
+console.log(a, b); // 3,2 
