9749ca99c0e8be6b17197f2a76469f01d807375a
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-06-09 19:32:12 -0300

    c

---
+++ aula-7/script.js
@@ -0,0 +1,7 @@
+
+let numero1 = prompt('digite um numero');
+let numero2 = prompt('digite um numero');
+numero1 = parseFloat(numero1);
+numero2 = Number(numero2);
+let resultado = numero1 + numero2;
+alert (resultado);
