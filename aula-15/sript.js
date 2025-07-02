739e975b52def25f8d1f4cc2ba15fe112dac9f64
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-02 17:01:25 -0300

    atualizar

--- aula-15/sript.js
+++ aula-15/sript.js
@@ -9,7 +9,7 @@ function saudacao(nome) {
   console.log(`Bom dia ${nome}!`);
 }
 
-const variavel = saudacao('luiz');
+let variavel = saudacao('luiz');
 
 console.log(variavel); // → undefined
 
@@ -25,24 +25,36 @@ console.log(variavel); // → "Bom dia luiz!"
 /* e agora ao mencionar a variavel, é determinado o valor retornado, entretanto sem que seja retornado um valor, sera undefined */
 
 function soma(x, y) {
-    const resultado = x + y;
+    let resultado = x + y;
     return resultado;
 }
 
 console.log(soma(2, 8)); // os numeros irao substituir o x e y
-console.log(resultado); // nao funciona pos meciona uma variavel no scopo local, se foi declarado lá, só funciona lá
+// console.log(resultado); // nao funciona pos meciona uma variavel no scopo local, se foi declarado lá, só funciona lá
 
-const resultado = soma(2,8); // dessa maneira ira funcionar 
+let resultado = soma(2,8); // dessa maneira ira funcionar 
 console.log (resultado);
 
 
-function soma(x = 2, y = 5) { // deterina um valor padrao
-    const resultado = x + y;
+function mais(x = 2, y = 5) { // deterina um valor padrao
+    let resultado = x + y;
     return resultado;
 }
 
-console.log(soma(2, 8)); // os numeros irao substituir o x e y
+console.log(mais(2, 8)); // os numeros irao substituir o x e y
 
 
-resultado = soma(2,8); 
+resultado = mais(2,8); 
 console.log (resultado);
+
+
+// ha tambem como colocar uma funcao dentro de outra funcao 
+
+let raiz = function (n) {
+    return n ** 0.5;
+
+}; // ponto e virgula pois a funcao esta dentro do const
+
+console.log(raiz(9));
+console.log(raiz(81));
+console.log(raiz(144));
