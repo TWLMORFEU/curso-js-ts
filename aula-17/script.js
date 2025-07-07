c0bf5b4ebdf0c2dd8ea9e4ba1a9eb0d8a7715252
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-07 15:43:54 -0300

    ~9

---
+++ aula-17/script.js
@@ -0,0 +1,7 @@
+/*
+Primitivos ( imutáveis - não pode mudar = alterar ) - string, number, boolean, undefined, null (bigint, symbol) 
+*/
+
+let nome = 'Rosa';
+nome = 'Saraiva';    // Mudando o valor da variável
+console.log(nome);
