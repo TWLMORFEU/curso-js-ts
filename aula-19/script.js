d3ac038e6f3ed44661324265261cdf46756b6e0d
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-10 19:05:56 -0300

    atualizar

---
+++ aula-19/script.js
@@ -0,0 +1,22 @@
+/*
+Operadores de comparação
+< - Menor que
+> - Maior que 
+>= - Maior que ou igual a
+<= - Menor que ou igual a
+== - Igualdade RECOMENDADO NÃO USAR (Valor)---(= Atribuição)
+=== - Igualdade estrita (Valor e tipo)
+!= - Diferente (Valor) RECOMENDADO NÃO USAR
+!== - Diferente estrito
+*/
+
+
+console.log(10 > 5); // True
+console.log(10 < 5); // False
+console.log(10 >= 5); // True, ate 5 for 10. 
+console.log(10 <= 5); // False 
+
+
+const num1 = 34;
+const num2 = 34;
+console.log(num1 == num2) // True, são iguais
