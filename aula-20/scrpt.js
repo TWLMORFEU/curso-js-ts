4062332f74be02b7c9f9963b47fed7fd7a8a6ad2
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-13 19:34:24 -0300

    atualizar

---
+++ aula-20/scrpt.js
@@ -0,0 +1,10 @@
+/*
+Operadores lògicos
+&& -> AND -> E -> Toas as expressões tem que ser verdadeiras para retornar True
+|| -> Or -> Ou
+! -> Not -> Não
+*/
+let expressao = true && true && false && true;
+console.log(expressao);
+expressao = true && true && true && true;
+console.log(expressao)
