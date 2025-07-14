b55f6897c4a65dfdd2ce3716039112b638b3905d
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-14 16:00:54 -0300

    atualizar

--- aula-20/scrpt.js
+++ aula-20/scrpt.js
@@ -5,6 +5,18 @@ Operadores lògicos
 ! -> Not -> Não
 */
 let expressao = true && true && false && true;
-console.log(expressao);
+console.log(expressao); // falsw
 expressao = true && true && true && true;
-console.log(expressao)
+console.log(expressao) // true
+
+let expressaor = false || true;
+/* (true + true = true)(true + false = true)(false + false = false) só retorna false quando todas forem falsas*/ 
+console.log(expressaor);
+
+
+const usuario = 'Luiz'; // form usuario digitou
+const senha =  '12s3d5'; // form usuario digitou 
+
+/* para logar, o usuario e senha teêm que corresponder aos dois dados  */
+const vaiLogar = usuario === 'Luiz' && senha ==='12s3d5';
+console.log(vaiLogar);
