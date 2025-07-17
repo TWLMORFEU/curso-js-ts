13f76166b6e8cd4f07432e74f807a5a87e761b19
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-17 15:50:00 -0300

    atualizar

---
+++ aula-22/script.js
@@ -0,0 +1,13 @@
+// if, else, if else. Estruturas conicionais 
+// if = se / else = senao
+let hora = 23;
+
+if ( hora > 6 && hora < 12) {
+    console.log('Bom Dia'); 
+ 
+}else if (hora > 12 && hora < 19) {
+    console.log('Boa Tarde');
+
+}else {
+    console.log('Boa Noite');
+}
