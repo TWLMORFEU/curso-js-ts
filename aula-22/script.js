2df1bfaa441bf20a1824ca7461809d4076ceda3c
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-17 15:59:08 -0300

    atualiza

--- aula-22/script.js
+++ aula-22/script.js
@@ -1,13 +1,16 @@
 // if, else, if else. Estruturas conicionais 
 // if = se / else = senao
-let hora = 23;
+let hora = 3;
 
 if ( hora > 6 && hora < 12) {
-    console.log('Bom Dia'); 
+    console.log('Olá, Um Bom Dia'); 
  
-}else if (hora > 12 && hora < 19) {
-    console.log('Boa Tarde');
+}else if (hora > 12 && hora < 18) {
+    console.log('Olá, Uma Boa Tarde');
 
 }else {
-    console.log('Boa Noite');
+    console.log('Olá,Boa Noite');
 }
+
+
+/* Começa com if = se, ao meio else if = senao se, e ao fim apenas senao*/
