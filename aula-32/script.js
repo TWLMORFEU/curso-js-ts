87fc7666d0459e35c18d1b38965ab1bdf347cddb
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-12 16:23:14 -0300

    inclusão de três diferentes métodos

--- aula-32/script.js
+++ aula-32/script.js
@@ -11,6 +11,27 @@ console.log("Linha 05");
 
 
 // i - index
-for (let i = 0; i <= 5;) {
-
+for (let i = 0; i <= 20; i+=10) {
+   console.log('Linha', i);
 }
+
+// i++ salto de uma casa
+// i+=2 -> Salto de duas casas
+// i+=10 -> Salto de dez casas
+
+
+for (let i = 500; i >= 400; i -= 10) {
+    console.log(`Linha ${i}`);
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    // const par = i % 2 === 0;
+    const par = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}
+
+// par recebe verdadeiro ou falso, se i for % 2 = 0 retorna true, e caso contrário false.
+
+
+
