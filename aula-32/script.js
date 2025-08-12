395eec21195a0b7bf4304d1891e062b980e0f6d7
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-12 15:34:56 -0300

    atualizar introdução

--- aula-32/script.js
+++ aula-32/script.js
@@ -1,2 +1,16 @@
 // ESTRUTURAS DE REPETIÇÃO
 
+/*
+console.log("Linha 0");
+console.log("Linha 01");
+console.log("Linha 02");
+console.log("Linha 03");
+console.log("Linha 04");
+console.log("Linha 05");
+*/
+
+
+// i - index
+for (let i = 0; i <= 5;) {
+
+}
