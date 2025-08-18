5575974b72dbcc73459b09285912e27ff854f7f6
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-18 17:05:35 -0300

    finalizando aula com criação de elementos ao html via js juntamente com estrutura de repetição e desestruturação

--- aula-33/script.js
+++ aula-33/script.js
@@ -1,5 +1,5 @@
 const elementos = [
-    {tag: 'p', texto: 'Frase 1'}, // 0
+    {tag: 'p', texto: 'Qualquer texto que você quiser.'}, // 0
     {tag: 'div', texto: 'Frase 2'}, // 1
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'},
@@ -11,6 +11,12 @@ const div = document.createElement('div');
 
 // Não da para usar menor ou igual, pois os elementos do js são iniciados a partir do zero e são 4 elementos
 for (let i = 0; i < elementos.length; i++) {
-    console.log(elementos[i]);
+    let { tag, texto } = elementos[i];
+    let tagCriada = document.createElement(tag);
+    // tagCriada.innerHTML = texto;
+
+    let textoCriado = document.createTextNode(texto)
+    div.appendChild(tagCriada);
 }
 // ele ira seguir a ordem do primeiro ao último, então quando acabar a primeira, vai para a segunda e assim sucessivamente
+container.appendChild(div)
