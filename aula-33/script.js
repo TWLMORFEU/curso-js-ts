2895a2df45aa5d54821bef64e38f8a3aff94908b
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-18 16:27:39 -0300

    atualizar

--- aula-33/script.js
+++ aula-33/script.js
@@ -1,8 +1,16 @@
-const elements = [
+const elementos = [
     {tag: 'p', texto: 'Frase 1'}, // 0
     {tag: 'div', texto: 'Frase 2'}, // 1
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'},
 ];
 
-const container = document.querySelector(''.container')
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+
+// Não da para usar menor ou igual, pois os elementos do js são iniciados a partir do zero e são 4 elementos
+for (let i = 0; i < elementos.length; i++) {
+    console.log(elementos[i]);
+}
+// ele ira seguir a ordem do primeiro ao último, então quando acabar a primeira, vai para a segunda e assim sucessivamente
