46d30d1f5109858b1318e40588632e81225eaa6d
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-21 14:46:57 -0300

    informações de Dom

---
+++ aula-34/script.js
@@ -0,0 +1,9 @@
+// DOM e a sua árvore
+
+
+/*
+Window no navegador ele contém todo o navegador dentro dele, como se ele fosse o pai de todos os elementos 
+
+
+window.document = html, daí temos um exemplo da árvore, por ser um elemento dentro do outro
+*/
