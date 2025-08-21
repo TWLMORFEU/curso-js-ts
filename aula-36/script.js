94fd8bf2999a900a788d504ba11c0ef43cf776e8
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-21 16:32:55 -0300

    atualizar conteúdo

--- aula-36/script.js
+++ aula-36/script.js
@@ -2,11 +2,11 @@
 
 
 //            01234---
-const nome = ['Luiz Otávio', 'Eduardo Miguel', 'Marcelo Adriano'];  // string inteirável
+const nome = ['Luiz Otávio', 'Eduardo Miguel', 'Marcelo Adriano'];  // string iterável
 
 
 for (let i = 0; i < nome.length; i++) {  // tem o indice
-   console.log(nome[i])
+   console.log(nome[i]);
 }
 // sera mostrado letra por letra a cada volta
 
@@ -14,8 +14,21 @@ for (let i in nome) {   // tem o indice mas simplificado
     console.log(nome[i]);
 }
 
-for (let valor of nome) { // nao tem indice apenas valor
-    
+for (let valor of nome) { // nao tem indice apenas valor   
     // dessa maneira, sera retornado o valor diretamente, e não o indice mesmo que sem colchetes
     console.log(valor);
 }
+
+// transmite em 3 parametros
+/*
+1- o valor dos indices
+2- o numero dos indices
+3- o array completo
+*/
+//                      1       2      3
+nome.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array);
+}); 
+
+
+
