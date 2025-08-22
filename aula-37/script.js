0c1ee6dc0f431757f36918177fbe3861ed87155f
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-22 19:50:51 -0300

    atualizar final de aula 37

--- aula-37/script.js
+++ aula-37/script.js
@@ -2,14 +2,17 @@
 para selecionar varios elementos dentro de um só, ou seja, uma coleção, o queryselector("") não é suficiente, entretanto, ao usar querselectorAll (""), todos os paragrafos por exemplo dentro de uma div são constatados. 
 essa nodelist é semelhante demais a um array porem não é a mesma coisa
 */
-const paragrafos = document.querySelector('.paraagrafos');
+const paragrafos = document.querySelector('.paragrafos');
 const ps = paragrafos.querySelectorAll('p');
 // ps.length = 4 pois são 4 paragrafos
 
 
-const estilosBody = getComputedStyle(document.body);
-
+const estilosBody = getComputedStyle(document.body); 
+// seleciona a cor do body
+const backgroundColorBody = estilosBody.backgroundColor.
+// captura a cor 
+console.log(backgroundColorBody)
 
 for (let p of ps) {
-    console.log(p);
+    p.style.backgroundColor = backgroundColorBody; // aplica a cor aos paragrafos
 }
