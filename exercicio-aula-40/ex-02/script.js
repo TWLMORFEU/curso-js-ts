5de9d0e3da139c50238591138ae9e565c9ce5d7f
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-28 12:38:16 -0300

    segundo exercício completo

---
+++ exercicio-aula-40/ex-02/script.js
@@ -0,0 +1,20 @@
+/*
+- Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem (number).
+Retorne true se a imagem estiver no modo paisagem
+*/
+
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const altura = Number((frm.ialtura.value));
+    const largura = Number((frm.ilargura.value));
+
+    function ePaisagem(altura, largura) {
+        return largura > altura
+    }
+
+    resp.innerText = (ePaisagem(altura, largura));
+});
