c04b25b73c4a67bd746b7bb357face48f0f372b0
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-09-02 17:08:32 -0300

    finalizando aula

--- aula-42/script.js
+++ aula-42/script.js
@@ -1,34 +1,34 @@
 // tratando e lançando erros (try, catch, finally)
 
-try {
-  // É executada o código quando não há erros
-} catch (e){
-  // É executada o código quando há erros
-} finally {
-  // Sempre será executado com ou sem erros
-};
+// try {
+//   // É executada o código quando não há erros
+// } catch (e){
+//   // É executada o código quando há erros
+// } finally {
+//   // Sempre será executado com ou sem erros
+// };
 
 
-try {
-    console.log("Abri uma gaveta");
-    console.log("Peguei a caderno");
-    console.log("Fechei a gaveta");
+// try {
+//     console.log("Abri uma gaveta");
+//     console.log("Peguei a caderno");
+//     console.log("Fechei a gaveta");
 
-    try {
-        console.log(b);
-    } catch(e) {
-        console.group("Deu erro")
-    }
+//     try {
+//         console.log(b);
+//     } catch(e) {
+//         console.group("Deu erro")
+//     }
 
-} catche (e) {
-    console.log("Tratando o erro");
-} finally {
-    console.log("FINALLY: Eu sempre sou executado");
-}
+// } catche (e) {
+//     console.log("Tratando o erro");
+// } finally {
+//     console.log("FINALLY: Eu sempre sou executado");
+// }
 
 
 
-// -------------------------------
+// --------------------------------
 
 // quando a condição estiver dentro de uma função é bom verificar invertido
 function retornaHora(data) {
@@ -39,3 +39,42 @@ function retornaHora(data) {
 }
 retornaHora(new Date()) // Faz nada pois é uma instância de Date
 retornaHora(); // Mostra (Não é) pois não é uma instância de Date
+
+
+
+// ----------------------------------
+
+
+
+function retornnaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instãnia de Date.');
+    }
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('Pt-Br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+
+        hour12: false // tira AM e PM
+    })   
+}
+
+try {
+    const dataa = new date("01-01-1970 12:58:12") 
+    const hora = retornnaHora(11);
+    console.log(hora);
+} catch (e) {
+    // Tratar erro
+    // console.log(e) mostrar erro do programa
+} finally {
+    console.log('Tenha um bom dia !')
+}
+// const dataa = new date("01-01-1970 12:58:12") 
+// const hora = retornnaHora(11); - Dar erro
+// console.log(hora);
+
