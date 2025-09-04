94c166303426713cc93e669e060eeb4ebc96c301
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-09-04 16:02:05 -0300

    atualização em estrutura de exercicio

---
+++ aula-44-/script.js
@@ -0,0 +1,23 @@
+// Gerando um timer
+
+let relogio = document.querySelector('#relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+/* Nova maneira de capturar um clique no botão */
+iniciar.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  relogio = setInternal(function() {
+    
+  }, 1000);
+});
+
+pausar.addEventListener("click", function(event) {
+  event.preventDefault();
+});
+
+zerar.addEventListener("click", function(event) {
+  event.preventDefault();
+});
