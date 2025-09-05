64993a1d8b496f6e89433963fa8b3f6695153f00
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-09-05 17:26:43 -0300

    atualizar exercicio

--- aula-44-/script.js
+++ aula-44-/script.js
@@ -26,12 +26,14 @@ function iniciaRelogio() {
 
 // Botão iniciar
 iniciar.addEventListener("click", function () {
+  relogio.classList.remove('pausado')
   clearInterval(timer); // evita criar múltiplos timers para o contador antigo, e recomeça de onde parou com o outro
   iniciaRelogio();
 });
 
 // Botão pausar
 pausar.addEventListener("click", function () {
+  relogio.classList.add('pausado')
   clearInterval(timer);
 });
 
@@ -40,4 +42,19 @@ zerar.addEventListener("click", function () {
   clearInterval(timer);
   segundos = 0;
   relogio.innerHTML = "00:00:00";
+});
+
+
+/* os numeros se mantem em seus valores, pois a variavel segundos se mantem ja que nao estamos redeclarando, mas sim, apenas manipulando */
+
+document.addEventListener('click', function(e) {
+    // console.log(e.target);  ver elemento que esta sendo clicado
+    const el = e.target; // o elemento que foi clicado, m especifico ira ir pra essa constante. Maneira de lhe dar com varios botaos em uma pagina
+
+   /* if(el.classList.container('zerar')) {
+        console.log('Você clicou em zerar');
+    }*/
+    //se o botão clicado for dessa classe ira mostrar a mensagem
+
+    
 });
