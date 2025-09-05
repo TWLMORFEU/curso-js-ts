5a9e94593cb605e69e41e4fea0426fb372c19a12
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-09-05 16:10:51 -0300

    atualizar codigo

--- aula-44-/script.js
+++ aula-44-/script.js
@@ -1,21 +1,43 @@
-// Gerando um timer
-
-let relogio = document.querySelector('#relogio');
+// Selecionando elementos
+const relogio = document.querySelector('#relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
-/* Nova maneira de capturar um clique no botão */
-iniciar.addEventListener("click", function(event) {
-  event.preventDefault();
+let segundos = 0;
+let timer;
 
-  
+// Função para formatar no estilo 00:00:00
+function criaHora(segundos) {
+  const data = new Date(segundos * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC'
+  });
+}
+
+// Função que inicia o contador
+function iniciaRelogio() {
+  timer = setInterval(function () {
+    segundos++;
+    relogio.innerHTML = criaHora(segundos);
+  }, 1000);
+}
+
+// Botão iniciar
+iniciar.addEventListener("click", function () {
+  clearInterval(timer); // evita criar múltiplos timers para o contador antigo, e recomeça de onde parou com o outro
+  iniciaRelogio();
 });
 
-pausar.addEventListener("click", function(event) {
-  event.preventDefault();
+// Botão pausar
+pausar.addEventListener("click", function () {
+  clearInterval(timer);
 });
 
-zerar.addEventListener("click", function(event) {
-  event.preventDefault();
+// Botão zerar
+zerar.addEventListener("click", function () {
+  clearInterval(timer);
+  segundos = 0;
+  relogio.innerHTML = "00:00:00";
 });
