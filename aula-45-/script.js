ea84706e8a1dfa250426158d1fcc2a5be6a339f2
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-10-02 15:24:01 -0300

    atualizar

--- aula-45-/script.js
+++ aula-45-/script.js
@@ -1,2 +1,29 @@
 // Lista de Tarefas
 
+const inputTarefa = document.querySelector(".input-tarefa");
+const btnTarefa = document.querySelector(".btn-tarefa");
+const tarefas = document.querySelector(".tarefas");
+
+
+function criaLi() {
+    const li = document.createElement('li'); // cria lista
+    return li;
+}
+
+
+
+function criaTarefa(textoInput) { // função recebe texto
+    const li = criaLi(); // passa a lista criada à função
+    li.innerText = textoInput; // mostra o valor de acordo com a função
+    tarefas.appendChild(li)
+}
+
+btnTarefa.addEventListener('click', function() {
+    if (!inputTarefa.value) return;
+
+    criaTarefa(inputTarefa.value);
+    // jogando o texto para uma função
+
+    console.log(inputTarefa.value);
+    // capturou o texto
+});
