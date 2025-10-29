1caa89fc573fdf83493cc2cd5c97e17d9f21344f
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-10-29 10:03:31 -0300

    introduzindo retorno de funções avançadas

---
+++ aula-48/script.js
@@ -0,0 +1,29 @@
+// Retorno de funções
+
+// return -> retorno de função -> termina a função
+
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(5, 2)); // 7
+
+
+function soma2(a, b) {
+    console.log(a, b);
+}
+soma2(5, 2);// 7
+
+
+
+document.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+}); // Muda a cor do body, entretanto não retorna nada
+
+
+
+function criaPessoa(nome, sobrenome) {
+    return { nome, sobrenome};
+}
+const p1 = criaPessoa('Luiz', 'Otávio');
+
+// mesma coisa de:
