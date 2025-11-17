b5d0d2163fa598fde866a1d7962888fe31201db2
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-11-17 18:14:00 -0300

    atualizar configurações em js

--- aula-54-/script.js
+++ aula-54-/script.js
@@ -1,3 +1,46 @@
 // Calculadora basica
 
 
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+
+
+
+
+       inicia() {
+        this.cliqueBotoes
+       },
+
+
+        cliqueBotoes() {
+         document.addEventListener('click', function(e) {
+           const el = e.target;
+
+           if(el.classList.contains('btn-num')) {
+               this. btnParaDisplay();
+           }
+        }.bind(this));
+       },
+
+       btnParaDisplay() {
+        
+       }
+
+    };
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia();
+
+
+
+
+
+/*
+.bind(this)); = uma maniplação do valor this, this sera a referencia do evento do scopo,porem para manter sua referencia riginal e mencionada dessa forma com bind no fim da função. Determinando que o responsável daquele scopo, que sera o this para o resto da estrutura ali por diante daquele scopo 
+*/
+
+/* 
+Para chamar uma chave dentro do objeto, exemplo calculadora e dentro dela executar uma chave deve-se chamar this.cliqueBotoes(); se chamar cliqeBotoes() não ira funcionar
+*/
