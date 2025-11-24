b047edcdc68e7b6222a4ade591937cdcd5240968
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-11-24 15:46:01 -0300

    finalizando aula de funções construtoras

--- aula-55/script.js
+++ aula-55/script.js
@@ -11,5 +11,28 @@
 
 
 function Pessoa(nome, sobrenome) {
-    this.nome 
+    // PRIVADAS -> inacessíveis por fora
+    const ID = 123456;
+    const metodoInterno = function () {
+
+    };
+
+    // Atributos ou Métodos púlicos
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+
+    this.metodo = function() {
+        console.log(this.nome + ': sou um método');
+    };
+
 }
+
+
+
+const p1 = new Pessoa('Luiz', 'Otávio');
+const p2 = new Pessoa('Marcos', 'Roxer');
+// NÃO ESQUECER DA UTILIZAÇÃO DO NEW
+console.log(p2.nome, p1.sobrenome)
+
+// O new ja pega o valor retornado pela função, e o new so funcionara corretamente acompanhado pelo this
