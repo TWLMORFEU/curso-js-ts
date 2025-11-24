c29860212bade32f8cddeb671ae2e9bf50eafc91
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-11-24 14:52:45 -0300

    iniciando funções construtoras

---
+++ aula-55/script.js
@@ -0,0 +1,15 @@
+// Funções Construtoras
+// Função Fábrica -> Função relativamente normal, onde retorna um objeto voluntariamente.
+// Função Construtora ->  Ja faz certas coisas automaticamente, cria e retorna o objeto.
+
+
+
+
+// OBS -> Função construtora, inicia com letra maiúscula em seu nome
+// Fábrica -> criaPessoa
+// Construtora -> Pessoa (new) | new é obrigatório
+
+
+function Pessoa(nome, sobrenome) {
+    this.nome 
+}
