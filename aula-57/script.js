3001d5b361e93d3947315a93209a13e869b1d959
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-11-26 16:09:22 -0300

    finalizado

--- aula-57/script.js
+++ aula-57/script.js
@@ -1,5 +1,13 @@
 // Funções recursivas
 // Associada com funcionalidade semelhate a unma estrutura de repetição
-function recursiva() {
 
+function recursiva(max) {
+   console.log(max);
+   if (max >= 1000000) return;
+   max++;
+   recursiva(max);
 }
+
+recursiva(0);
+
+// Função de recursividade -> uma função que se chama de volta, ao usala, se preocupe quando ela ira parar.
