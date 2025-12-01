9bcd2f7d36ffa896d3c17d41871df179c79a44bd
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-12-01 15:39:05 -0300

    exemplo de array construtor

--- aula-60/script.js
+++ aula-60/script.js
@@ -1,1 +1,19 @@
 // Inicialiando Arrays Avançados
+// Revisando o básico
+
+
+//                  0         1        2
+const nomes = [ "Eduardo", 'Maria', 'Joana']
+nomes[2] = 'João';
+delete nomes[2];
+console.log(nomes);
+
+
+const animais = [] //  array literal
+
+
+
+const sobrenomes = new Array('Silva, Vieira, Castro'); // Array Construtor
+nomes[2] = 'Ferrari';
+delete sobrenomess[2];
+console.log(sobrenomes)
