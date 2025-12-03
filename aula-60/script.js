295b52d9c4843a7320eee373466c7e2a8d2fda86
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-12-03 15:42:02 -0300

    observações de push e pop e tambem shift e unshift

--- aula-60/script.js
+++ aula-60/script.js
@@ -24,12 +24,18 @@ console.log(sobrenomes)
 const Rei = ['Eduardo', 'Maria', 'Joana'];
 const newRei = [...nomes]; // cópia, portanto um se altera e o outro não. Caso fosse somente = teria mudado os dois
 
-newRei.pop();
+newRei.pop();// remove o  último elemento do array
 console.log(Rei);
 console.log(newRei);
 
 
-
-
 const Animais = ['cachorro', 'leão', 'capivara']
-console.log(Animais.length);
+console.log(Animais.length); // 3 pis tem 3 elementos
+
+// .unshift(josé) adiciona no começo
+// .shift() tira o primeiro elemento do rray e os demais substituem a casa do outro o 3 vira 2 e 2 vira 1. Não recomendado para arrays grandes
+
+
+// o .pop() elimia o ultimo elemento do array
+// .push(tadeu) adiciona um indice 
+// mais usados
