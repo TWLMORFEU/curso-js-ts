44054248d35a8649c55ba78554592b5890bc659b
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-12-02 22:23:53 -0300

    estrategia de copia em array

--- aula-60/script.js
+++ aula-60/script.js
@@ -15,5 +15,21 @@ const animais = [] //  array literal
 
 const sobrenomes = new Array('Silva, Vieira, Castro'); // Array Construtor
 nomes[2] = 'Ferrari';
-delete sobrenomess[2];
+delete sobrenomes[2];
 console.log(sobrenomes)
+
+
+
+//               0         1        2
+const Rei = ['Eduardo', 'Maria', 'Joana'];
+const newRei = [...nomes]; // cópia, portanto um se altera e o outro não. Caso fosse somente = teria mudado os dois
+
+newRei.pop();
+console.log(Rei);
+console.log(newRei);
+
+
+
+
+const Animais = ['cachorro', 'leão', 'capivara']
+console.log(Animais.length);
