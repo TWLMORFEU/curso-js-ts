f32e460d23e7ab14d3bdb524d50252875aed7895
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-12-05 16:31:49 -0300

    como funciona a estrutura splice

---
+++ aula-61/script.js
@@ -0,0 +1,12 @@
+// Método Splice
+// faz tudo referente ao fim do resumo básico anterior, também mexendo o array principal
+ 
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// nomes.splice(0,1,elemen1, elemen2)
+// 0 -> indice que quer quer iniciar
+// 1 -> quantos elementos quer remover
+// elemen1,2,3... - em fim elementos que quer adicionar separados por vírgula
+
+
+// simular a função pop (remover o último índice)
