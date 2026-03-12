f842f02bb38a54a77bc3588d98cafdd854a59c0b
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2026-03-12 15:44:30 -0300

    método de filtro por callback

--- aula-63/script.js
+++ aula-63/script.js
@@ -2,4 +2,29 @@
 // Filter: filtrar Map: Modificar Reduce: Reduz
 
 // Retorne  os números maiores que 10
-const numeros = [5, 50, 80, 1, 2, 3, 4, 8, 7, 11, 15, 22, 37];
+const num1 = [5, 50, 80, 1, 2, 3, 4, 8, 7, 11, 15, 22, 37];
+
+
+
+
+
+// Função longa: 
+
+//         0   1  2   3  4  5  ...
+const num2 = [9, 60, 80, 2, 3, 1, 6, 43, , 11, 15, 22, 77];
+
+function callbackFilter(valor) {
+    if (valor > 10) {
+
+
+ //      return true;
+ //   } else {
+ //       return false;
+ // JA SERA RETORNADO TRUE OU FALSE EM UMA CONDIÇÃO, PORTANTO NÃO É NECESSARIO
+    }
+}
+
+
+const numerosFiltrados = num2.filter(callbackFilter); // função de callback
+
+console.log(numerosFiltrados);
