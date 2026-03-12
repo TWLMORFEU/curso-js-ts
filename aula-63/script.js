dfb895b7cac5095559291ea92efc8c4915e829ac
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2026-03-12 15:58:31 -0300

    Exemplos com, funções anonimas e arrow function

--- aula-63/script.js
+++ aula-63/script.js
@@ -13,18 +13,38 @@ const num1 = [5, 50, 80, 1, 2, 3, 4, 8, 7, 11, 15, 22, 37];
 //         0   1  2   3  4  5  ...
 const num2 = [9, 60, 80, 2, 3, 1, 6, 43, , 11, 15, 22, 77];
 
+const numerosFiltrados = num2.filter(callbackFilter); // função de callback
+
 function callbackFilter(valor) {
-    if (valor > 10) {
+    return valor > 10; 
 
 
  //      return true;
  //   } else {
  //       return false;
  // JA SERA RETORNADO TRUE OU FALSE EM UMA CONDIÇÃO, PORTANTO NÃO É NECESSARIO
-    }
+
 }
 
-
-const numerosFiltrados = num2.filter(callbackFilter); // função de callback
-
 console.log(numerosFiltrados);
+
+
+
+// -----------------------------------------------------------
+// função anônima
+
+const numeroFiltrado = num2.filter(function(valor) {
+    return valor > 10;
+});
+
+console.log(numeroFiltrado);
+
+
+// -----------------------------------------------------------
+// Arrow function
+
+numeroFiltrado = num2.filter((valor) => {
+    return valor > 10;
+});
+
+console.log(numeroFiltrado);
