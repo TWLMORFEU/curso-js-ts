944633fdf8e7c8f37b269b022b0f7bb0b4b07f8c
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2026-04-01 15:05:04 -0300

    utilizando mapeamento de array

--- aula-64/script.js
+++ aula-64/script.js
@@ -1,2 +1,19 @@
 // Mapeando o array 
+// altera o valor, recebe o mesmo tamanho mas o seus valores se alteram
 
+
+// dobre os números 
+
+//               0  1   2   3  4  5 ...
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numeroEmdobro = numeros.map(function(valor, indice, array) {
+    // mostra c/d valor, indice + array
+    console.log(valor, indice, array);
+});
+
+const numeroS = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numeroEmDobro = numeroS.map(function(valor) {
+   // return `Edu -> ${valor}`;
+   return valor * 2 
+});
+console.log(numeroEmDobro);
